b3ad71efef58f79ed3498bf3aefeae19cd87f42e
Avishkar Patil <67785446+avipatilpro@users.noreply.github.com>
2021-05-15 23:43:27 +0530

    Initial Commit

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,4 +1,4 @@
-# (c) @EverythingSuckz | @AbirHasan2005
+# (c) @AvishkarPatil | @EverythingSuckz
 
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -10,7 +10,7 @@ class Var(object):
     API_ID = int(getenv('API_ID'))
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'AHFile2LinkBot'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'F2LxBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
